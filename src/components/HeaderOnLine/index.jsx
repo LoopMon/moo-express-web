e77268f21d92125daa3d5e075e9d82f8e492f518
João Lucas <introducaoteste@gmail.com>
2024-05-21 06:36:56 -0300

    Correção de CSS e erros

--- src/components/HeaderOnLine/index.jsx
+++ src/components/HeaderOnLine/index.jsx
@@ -1,16 +1,18 @@
+import { useState } from "react"
 import Logo from "../../assets/logo.png"
 import { Link } from "react-router-dom"
 import "./style.css"
 
 function HeaderOnLine() {
+    const [menuOpened, setMenuOpened] = useState(false)
+
     return (
         <header id="header-offline">
             <div className="logo">
                 <img src={Logo} alt="Logo moo express" />
             </div>
-            <div className="nav-bar">
-                {" "}
-                {/* Barra de Pesquisa */}
+            {/* Barra de Pesquisa */}
+            <div className="search-bar">
                 <div className="bar">
                     <input
                         type="text"
@@ -20,23 +22,23 @@ function HeaderOnLine() {
                         <i className="bi bi-search"></i>
                     </a>
                 </div>
-            </div>{" "}
+            </div>
             {/* Fim Barra de Pesquisa */}
-            <nav>
-                <a href="">
+            <nav className={menuOpened ? "open" : ""}>
+                <Link to="/myads">
                     <i className="bi bi-grid"></i>
                     <span>Meus anúncios</span>
-                </a>
+                </Link>
 
-                <a href="">
+                <Link to="/chat">
                     <i className="bi bi-chat-text"></i>
                     <span>Chat</span>
-                </a>
+                </Link>
 
-                <a href="">
+                <Link to="/notification">
                     <i className="bi bi-bell"></i>
                     <span>Notificações</span>
-                </a>
+                </Link>
 
                 <Link to="" id="entrar">
                     <span>{localStorage.getItem("logado")}</span>
@@ -46,6 +48,19 @@ function HeaderOnLine() {
                     <span>Sair</span>
                 </Link>
             </nav>
+
+            <div
+                className="menu"
+                onClick={() => {
+                    setMenuOpened(!menuOpened)
+                }}
+            >
+                {menuOpened ? (
+                    <i className="bi bi-x-lg"></i>
+                ) : (
+                    <i className="bi bi-list"></i>
+                )}
+            </div>
         </header>
     )
 }
--- src/components/PainelAds/index.jsx
+++ src/components/PainelAds/index.jsx
@@ -1,24 +1,83 @@
+import { useState, useEffect } from "react"
 import "./style.css"
 
 function PainelAds({ ads }) {
+    const [selectActive, setSelectActive] = useState(0)
+    const [adsArray, setAdsArray] = useState(ads)
+
+    useEffect(() => {
+        setAdsArray(
+            adsArray.map((ad) => {
+                return {
+                    id: ad.id,
+                    src: ad.src,
+                    alt: ad.alt,
+                    actived: selectActive === ad.id ? true : false,
+                }
+            }),
+        )
+    }, [])
+
+    const toggleSelected = () => {
+        const banner = document.querySelector(".banner")
+        banner.classList.remove("ativo")
+
+        if (selectActive < 0) {
+            setSelectActive(adsArray.length - 1)
+        } else if (selectActive >= adsArray.length) {
+            setSelectActive(0)
+        }
+
+        setAdsArray(
+            adsArray.map((ad) => {
+                return {
+                    id: ad.id,
+                    src: ad.src,
+                    alt: ad.alt,
+                    actived: selectActive === ad.id ? true : false,
+                }
+            }),
+        )
+    }
+
+    const bannerElements = adsArray.map((ad) => (
+        <div className={`banner ${ad.actived ? "ativo" : ""}`} key={ad.id}>
+            <img src={ad.src} alt={ad.alt} />
+        </div>
+    ))
+
+    const pointsElements = adsArray.map((ad) => (
+        <span
+            className={`ponto ${ad.actived ? "ativo" : ""}`}
+            key={ad.id}
+        ></span>
+    ))
+
     return (
-        <div className="banner">
-            <div className="painels">
-                {ads.map((ad) => (
-                    <div className="painel">
-                        <img src={ad} alt="" />
-                    </div>
-                ))}
-            </div>
+        <div id="painel">
+            <div className="banners">{bannerElements}</div>
 
-            <span id="left">
-                <i class="bi bi-caret-left-fill"></i>
+            <span
+                id="left"
+                onClick={() => {
+                    setSelectActive(selectActive - 1)
+                    toggleSelected()
+                }}
+            >
+                <i className="bi bi-caret-left-fill"></i>
             </span>
 
-            <span id="right">
-                <i class="bi bi-caret-right-fill"></i>
+            <span
+                id="right"
+                onClick={() => {
+                    setSelectActive(selectActive + 1)
+                    toggleSelected()
+                }}
+            >
+                <i className="bi bi-caret-right-fill"></i>
             </span>
-            <div className="pontos"></div>
+
+            <div className="pontos">{pointsElements}</div>
         </div>
     )
 }
